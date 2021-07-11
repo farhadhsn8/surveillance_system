a312b71fe1c023d8e88b34c783c36758f09bbc3c
farhadhsn <farhadhsn8@gmail.com>
2021-07-11 19:59:50 +0430

    save image and insert to DB

--- prj/sensor/4.py
+++ prj/sensor/4.py
@@ -1,5 +1,16 @@
 import cv2 
 import func 
+import datetime
+
+PICTURE_TIME = 0.5
+catCounter = 0
+bodyCounter = 0
+faceCounter = 0
+
+
+catTime = datetime.datetime.now() - datetime.timedelta(minutes = PICTURE_TIME)
+bodyTime = datetime.datetime.now() - datetime.timedelta(minutes = PICTURE_TIME)
+faceTime = datetime.datetime.now() - datetime.timedelta(minutes = PICTURE_TIME)
 
 cat_classifier = cv2.CascadeClassifier('data/haarcascade_frontalcatface.xml') 
 body_classifier = cv2.CascadeClassifier('data/haarcascade_fullbody.xml') 
@@ -15,14 +26,20 @@ while 1:
   
     # reads frames from a camera 
     ret, img = cap.read() 
-  
+    cv2.imshow('img',img)
+
+
     # convert to gray scale of each frames 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-  
-    func.fullBody(body_classifier,gray,img)
-    func.cat(cat_classifier,gray,img)
-    func.face(face_classifier,gray,img)
-    func.upperBody(ubody_classifier,gray,img)
+    
+    t_15mins_ago = datetime.datetime.now() - datetime.timedelta(minutes = PICTURE_TIME)
+    if(catTime < t_15mins_ago):
+        catTime , catCounter = func.cat(cat_classifier,gray,img,catCounter,catTime)
+    if(bodyTime < t_15mins_ago):
+        bodyTime , bodyCounter = func.fullBody(body_classifier,gray,img , bodyCounter,bodyTime)
+        bodyTime , bodyCounter = func.upperBody(ubody_classifier,gray,img , bodyCounter,bodyTime)
+    if(faceTime < t_15mins_ago):
+        faceTime , faceCounter = func.face(face_classifier,gray,img , faceCounter,faceTime)
     # func.body(img)
     # Wait for Esc key to stop 
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- prj/sensor/func.py
+++ prj/sensor/func.py
@@ -1,68 +1,157 @@
 import cv2 
+import datetime
+import mysql.connector
+from numpy import random
 
-def cat(cat_classifier,gray,img):
+
+
+
+def cat(cat_classifier,gray,img,catCounter ,catTime):
     # Detects faces of different sizes in the input image 
     cat = cat_classifier.detectMultiScale(gray, 1.3, 5) 
-
     for (x,y,w,h) in cat: 
         # To draw a rectangle in a face 
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+
+    if(len(cat) != 0):
+        catCounter += 1
+    if(catCounter >= 3):
+        print(cat)
+        catCounter=0
+        catTime = datetime.datetime.now()
+        saveEvent(img ,catTime ,"cat" )
+
     # Display an image in a window 
     cv2.imshow('img',img) 
+    return catTime , catCounter
 
 
 
 
 
-
-def fullBody(body_classifier,gray,img):
+def fullBody(body_classifier,gray,img,bodyCounter ,bodyTime):
     # Detects faces of different sizes in the input image 
     bodies = body_classifier.detectMultiScale(gray)
     
     # Extract bounding boxes for any bodies identified
     for (x,y,w,h) in bodies:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 90, 120), 2)
+
+    if(len(bodies) != 0):
+        bodyCounter += 1
+    if(bodyCounter >= 3):
+        print(bodies)
+        bodyCounter=0
+        bodyTime = datetime.datetime.now()
+        saveEvent(img ,bodyTime ,"body" )
+
     cv2.imshow('img', img)
+    return bodyTime , bodyCounter 
 
 
-def upperBody(ubody_classifier,gray,img):
+
+
+
+
+def upperBody(ubody_classifier,gray,img,bodyCounter ,bodyTime):
     # Detects faces of different sizes in the input image 
     ubodies = ubody_classifier.detectMultiScale(gray)
-    
+
+
     # Extract bounding boxes for any bodies identified
     for (x,y,w,h) in ubodies:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    cv2.imshow('img', img)
-  
+
+    if(len(ubodies) != 0):
+        bodyCounter += 1
+    if(bodyCounter >= 3):
+        print(ubodies)
+        bodyCounter=0
+        bodyTime = datetime.datetime.now()
+        saveEvent(img ,bodyTime , "body")
+
+    cv2.imshow('img', img )
+    return bodyTime , bodyCounter
 
 
-def face(face_classifier,gray,img):
+
+def face(face_classifier,gray,img,faceCounter , faceTime):
     # Detects faces of different sizes in the input image 
     faces = face_classifier.detectMultiScale(gray, 1.0485258, 6)
-    
+
     # Extract bounding boxes for any bodies identified
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (40, 255, 20), 2)
+
+    if(len(faces) != 0):
+        faceCounter += 1
+    if(faceCounter >= 3):
+        print(faces)
+        faceTime = datetime.datetime.now()
+        faceCounter=0
+        saveEvent(img ,faceTime , "face")
+
     cv2.imshow('img', img) 
+    return faceTime , faceCounter
 
 
 
-def body(img):
-    hog = cv2.HOGDescriptor() 
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
-# Detecting all humans 
-    (humans, _) = hog.detectMultiScale(img,  
-                                        winStride=(5, 5), 
-                                        padding=(0, 0), 
-                                        scale=1.21)
-    # getting no. of human detected
-    # print('Human Detected : ', len(humans))
+
+
+
+def saveEvent(img , time ,type):
+
+    mydb = mysql.connector.connect(
+     host="localhost",
+     user="root",
+     password="",
+     port = 3308 ,
+     database="surveillance_system"
+    )
+
+    address = 'images/' + type + '_' + str(random.randint(100008000)) + '.png'
+    status = cv2.imwrite(address , img)
+    print(status)
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO events ( image , created_at ,type ) VALUES (%s, %s , %s)"
+    val = ( address , time , type)
+    mycursor.execute(sql, val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def body(img):
+#     hog = cv2.HOGDescriptor() 
+#     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
+# # Detecting all humans 
+#     (humans, _) = hog.detectMultiScale(img,  
+#                                         winStride=(5, 5), 
+#                                         padding=(0, 0), 
+#                                         scale=1.21)
+#     # getting no. of human detected
+#     # print('Human Detected : ', len(humans))
     
-    # Drawing the rectangle regions
-    for (x, y, w, h) in humans: 
-        cv2.rectangle(img, (x, y),  
-                    (x + w, y + h),  
-                    (0, 0, 255), 2) 
+#     # Drawing the rectangle regions
+#     for (x, y, w, h) in humans: 
+#         cv2.rectangle(img, (x, y),  
+#                     (x + w, y + h),  
+#                     (0, 0, 255), 2) 
     
-    # Displaying the output Image 
-    cv2.imshow("img", img) 
+#     # Displaying the output Image 
+#     cv2.imshow("img", img) 
+
+
+
+
+
+
