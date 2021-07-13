9da5b9fb2460ced9c73a3beb607c4c2ce09282ac
farhadhsn <farhadhsn8@gmail.com>
2021-07-13 21:17:51 +0430

    api was created

---
+++ prj/controller/database/migrations/2021_07_13_155203_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string("type");
+            $table->text("image");
+            $table->binary("done")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
