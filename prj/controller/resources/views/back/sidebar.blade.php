be711fd0690ccad81bbaa62b5697e0eb07de1026
farhadhsn <farhadhsn8@gmail.com>
2021-07-19 20:14:22 +0430

    ui

--- prj/controller/resources/views/back/sidebar.blade.php
+++ prj/controller/resources/views/back/sidebar.blade.php
@@ -14,83 +14,56 @@
       </li>
       <li class="nav-item nav-category">منوی اصلی</li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="menu-icon typcn typcn-document-text"></i>
-          <span class="menu-title">کنترل پنل</span>
-        </a>
-      </li>
-      <li class="nav-item">
         <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <i class="menu-icon typcn typcn-coffee"></i>
-          <span class="menu-title">صفحات </span>
+          <span class="menu-title"> حساب کاربری </span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
             <li class="nav-item">
-              <a class="nav-link" href="#"> کاربران</a>
+              <a class="nav-link" href="#">کاربران</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">دسته بندی ها</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">مطالب</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">نظرات</a>
-            </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">تنظیمات حساب</a>
+              </li>
           </ul>
         </div>
       </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/charts/chartjs.html">
+                <i class="menu-icon typcn typcn-th-large-outline"></i>
+                <span class="menu-title">رخداد و اقدام</span>
+            </a>
+        </li>
       <li class="nav-item">
-        <a class="nav-link" href="pages/forms/basic_elements.html">
-          <i class="menu-icon typcn typcn-shopping-bag"></i>
-          <span class="menu-title">المانهای فرم</span>
+        <a class="nav-link"  data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic">
+          <i class="menu-icon typcn typcn-coffee"></i>
+          <span class="menu-title">پرس و جو</span>
+          <i class="menu-arrow"></i>
         </a>
+          <div class="collapse" id="ui-basic1">
+              <ul class="nav flex-column sub-menu">
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">لیست تمام رخداد ها</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">لیست رخداد های اقدام نشده</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">جستوجو بر اساس تاریخ</a>
+                  </li>
+              </ul>
+          </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/charts/chartjs.html">
-          <i class="menu-icon typcn typcn-th-large-outline"></i>
-          <span class="menu-title">نمودارها</span>
-        </a>
-      </li>
+
       <li class="nav-item">
         <a class="nav-link" href="pages/tables/basic-table.html">
           <i class="menu-icon typcn typcn-bell"></i>
-          <span class="menu-title">جدولها</span>
+          <span class="menu-title">خروج</span>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/icons/font-awesome.html">
-          <i class="menu-icon typcn typcn-user-outline"></i>
-          <span class="menu-title">آیکونها</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-          <i class="menu-icon typcn typcn-document-add"></i>
-          <span class="menu-title">صفحات</span>
-          <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="auth">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item">
-              <a class="nav-link" href="pages/samples/blank-page.html"> صفحه ی خالی </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/samples/login.html"> ورود </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/samples/register.html"> ثبت نام </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/samples/error-404.html"> 404 </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/samples/error-500.html"> 500 </a>
-            </li>
-          </ul>
-        </div>
-      </li>
+
+
     </ul>
   </nav>
